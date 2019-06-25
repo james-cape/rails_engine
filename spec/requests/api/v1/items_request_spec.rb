9f79c0a72264e147d30dee54fd922ed4c6856c79
James Cape <james.cape@gmail.com>
2019-06-25 15:24:52 -0600

    Adds item serializer and show method

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -5,10 +5,21 @@ describe "Items API" do
     merchant_1 = create(:merchant)
     create_list(:item, 3, merchant: merchant_1)
 
-    get '/api/v1/items'
+    get '/api/v1/items.json'
 
     expect(response).to be_successful
     items = JSON.parse(response.body)
-    expect(items.count).to eq(3)
+    expect(items["data"].count).to eq(3)
+  end
+
+  it "gets item by id" do
+    merchant_1 = create(:merchant)
+    id = create(:item, merchant: merchant_1).id
+
+    get "/api/v1/items/#{id}.json"
+
+    expect(response).to be_successful
+    item = JSON.parse(response.body)
+    expect(item["data"]["id"].to_i).to eq(id)
   end
 end
