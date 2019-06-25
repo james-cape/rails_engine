900bf81dcd76a676ffc019dbe0f4f5db0e0fd794
James Cape <james.cape@gmail.com>
2019-06-24 22:06:46 -0600

    Parses and tests items api

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -2,7 +2,12 @@ require 'rails_helper'
 
 describe "Items API" do
   it "sends a list of items" do
-    create_list(:item, 3)
+    merchant_1 = create(:merchant)
+    # customer_1 = create(:customer)
+    # invoice_1 = create(:invoice, merchant: merchant_1, customer: customer_1)
+    #
+    # create_list(:transaction, 3, invoice: invoice_1)
+    create_list(:item, 3, merchant: merchant_1)
 
     get '/api/v1/items'
 
