4911271a0f61af4da64e36f2577ce63944aac14e
James Cape <james.cape@gmail.com>
2019-06-25 15:45:55 -0600

    Adds transaction serializer and show method. All tables are serialized

--- app/controllers/api/v1/transactions_controller.rb
+++ app/controllers/api/v1/transactions_controller.rb
@@ -1,6 +1,9 @@
 class Api::V1::TransactionsController < ApplicationController
   def index
-    render json: Transaction.all
+    render json: TransactionSerializer.new(Transaction.all)
   end
 
+  def show
+    render json: TransactionSerializer.new(Transaction.find(params[:id]))
+  end
 end
--- spec/requests/api/v1/transactions_request_spec.rb
+++ spec/requests/api/v1/transactions_request_spec.rb
@@ -8,10 +8,25 @@ describe "Transactions API" do
 
     create_list(:transaction, 3, invoice: invoice_1)
 
-    get '/api/v1/transactions'
+    get '/api/v1/transactions.json'
 
     expect(response).to be_successful
     transactions = JSON.parse(response.body)
-    expect(transactions.count).to eq(3)
+    expect(transactions["data"].count).to eq(3)
   end
+
+  it "gets transaction by id" do
+    merchant_1 = create(:merchant)
+    customer_1 = create(:customer)
+    invoice_1 = create(:invoice, merchant: merchant_1, customer: customer_1)
+
+    id = create(:transaction, invoice: invoice_1).id
+
+    get "/api/v1/transactions/#{id}.json"
+
+    transaction = JSON.parse(response.body)
+    expect(response).to be_successful
+    expect(transaction["data"]["id"].to_i).to eq(id)
+  end
+
 end
