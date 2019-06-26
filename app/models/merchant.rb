9d0473cd2ae2d342f5642b3d94aff29df1b2a366
James Cape <james.cape@gmail.com>
2019-06-25 21:18:32 -0600

    Loads a collection of invoices associated with one customer

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,4 +1,18 @@
 class Merchant < ApplicationRecord
   has_many :invoices
   has_many :items
+
+  def self.most_revenue(merchant_limit)
+    select('merchants.*, SUM(invoice_items.quantity * invoice_items.unit_price) AS revenue)')
+    .joins(:invoices)
+    .joins(:transactions)
+    .joins(:invoice_items)
+    .where('transactions.result = ?', "success")
+    .group(:id)
+    .order('revenue DESC')
+    .limit(merchant_limit)
+
+
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,16 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
+
+      namespace :merchants do
+        # resources :most_revenue, only: [:index]
+        get '/most_revenue', to: 'most_revenue#index'
+      end
+
+      get '/customers/:id/invoices', to: 'customers/invoices#index'
+      # resources :customers,     only: [:index, :show] do
+      #   resources :invoices, only: :index
+      # end
       resources :customers,     only: [:index, :show]
       resources :invoice_items, only: [:index, :show]
       resources :invoices,      only: [:index, :show]
