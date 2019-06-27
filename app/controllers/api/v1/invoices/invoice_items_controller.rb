338ffd6881d596ce70d1bb8388e1dc395dfef4ad
James Cape <james.cape@gmail.com>
2019-06-26 18:23:21 -0600

    Changes .where in controller to .find

--- app/controllers/api/v1/invoices/invoice_items_controller.rb
+++ app/controllers/api/v1/invoices/invoice_items_controller.rb
@@ -1,7 +1,5 @@
 class Api::V1::Invoices::InvoiceItemsController < ApplicationController
   def index
-    ####### Why doesn't this code work?
-    # render json: InvoiceItemSerializer.new(Invoice.find(params[:id]).invoice_items)
-    render json: InvoiceItemSerializer.new(InvoiceItem.where(invoice_id: params[:id]))
+    render json: InvoiceItemSerializer.new(Invoice.find(params[:id]).invoice_items)
   end
 end
--- app/controllers/api/v1/items/invoice_items_controller.rb
+++ app/controllers/api/v1/items/invoice_items_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::Items::InvoiceItemsController < ApplicationController
   def index
-    render json: InvoiceItemSerializer.new(InvoiceItem.where(item_id: params[:id]))
+    render json: InvoiceItemSerializer.new(Item.find(params[:id]).invoice_items)
   end
 end
